83c18daf590f3e5de1bb7c7e5f3bf5e71a761925
LoNGER-2111 <96116356+LoNGER-2111@users.noreply.github.com>
2024-05-11 22:30:08 +0700

    push code

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,8 +2,9 @@ const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
   origin: (origin, callback) => {
+    // !origin condition is for dev env when origin is undefined
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
+      callback(null, true); // null represents no error thrown
     } else {
       callback(new Error("Not allowed by CORS"));
     }
